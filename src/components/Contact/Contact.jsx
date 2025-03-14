2e99339a38f8e807be1a63ce46d76bb920766f70
thulasisri <thulasisribalamurugan@gmail.com>
2025-03-14 09:32:08 +0530

    updated portfolio design

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './contact.css'
+import './Contact.css'
 import theme_pattern from '../../assets/theme_pattern.svg'
 import mail_icon from '../../assets/mail_icon.svg'
 import call_icon from '../../assets/call_icon.svg'
